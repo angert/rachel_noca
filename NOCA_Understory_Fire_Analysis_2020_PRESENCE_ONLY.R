31f2e210700714906d79d2a78031727b6b59ef61
rachelnorawilson <rachelnora.wilson@gmail.com>
2020-11-19 14:45:18 -0800

    Removed this additional line for testing.

--- NOCA_Understory_Fire_Analysis_2020_PRESENCE_ONLY.R
+++ NOCA_Understory_Fire_Analysis_2020_PRESENCE_ONLY.R
@@ -6,8 +6,6 @@
 
 ###### 2020 updates for manuscript flagged as such
 
-# Now I'm testing what it looks like to push multiple commits.
-
 #### Always do this first #####
 setwd("/Users/rachelwilson/Dropbox/Cascades resurveys/NCCO/2015/Data_entry")
 library(reshape2)
