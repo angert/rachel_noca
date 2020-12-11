5d9aecf9779d0162d805f835d24cdacbd1469e5b
rachelnorawilson <rachelnora.wilson@gmail.com>
2020-12-11 12:02:22 -0800

    Creating new script for presence analyses

---
+++ 2_presence_analyses.R
@@ -0,0 +1,14 @@
+# Created: Dec. 11, 2020
+# Updated: Dec. 11, 2020
+
+# This script will be used to undertake the PRESENCE analyses
+
+# IMPORTANT NOTE: unless otherwise indicated, always use Understory_All.csv for these analyses as it is the ONLY file with up-to-date corrections.
+
+# Packages needed:
+
+##TODO add packages here
+
+
+#### STEP 1: Import data ####
+
