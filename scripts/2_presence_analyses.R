396a4aed8b192dd07ff7eedc68290fbbd0b1f544
rachelnorawilson <rachelnora.wilson@gmail.com>
2020-12-11 14:47:53 -0800

    Beginning of for-loop: model averaging for fires vs no fires

--- scripts/2_presence_analyses.R
+++ scripts/2_presence_analyses.R
@@ -26,5 +26,29 @@ species.list <- factor(species.list)
 
 coeff.SPEC<-list()
 
-for(S in 1:length(species.list))
+for(S in 1:length(species.list)) {
+  
+  # Create subset for species of interest S
+  und.presence.SPEC = subset(und.presence, Species.Code == levels(species.list)[S])
+  und.presence.SPEC$Fires <- relevel(und.presence.SPEC$Fires, ref="Unburned")
+  und.presence.SPEC$Elevation.m2 <- und.presence.SPEC$Elevation.m^2 #TODO better than I()?
+  und.presence.SPEC <- und.presence.SPEC[complete.cases(und.presence.SPEC), ] #Just in case
+  
+  # Did the species occur in burned plots 5+ times?
+  num.burns <- table(und.presence.SPEC$Pres.Abs, und.presence.SPEC$Fires, und.presence.SPEC$Data.Type)
+  
+  # If yes, include fire as a predictor in global model:
+  if(num.burns[2,2,1] >= 5 | num.burns[2,2,2] >= 5) {
+    mod.globfi <- glm(Pres.Abs ~ Data.Type * (Elevation.m + Elevation.m2) * Fires, data = und.presence.SPEC, family = "binomial", na.action = na.fail) #Global fire model
+    options(warn = 1)
+    dredge.globfi <- dredge(mod.globfi, rank = AIC, subset = dc(Elevation.m, Elevation.m2))
+    avg.globfi <- model.avg(dredge.globfi, subset = delta <= 2)
+  }
+  
+  # If no, exclude fire from global model:
+  if(num.burns[2,2,2] < 5 | num.burns[2,2,1] < 5) {
+    mod.globnofi <- glm(Pres.Abs ~ Data.Type * (Elevation.m + Elevation.m2), data = und.presence.SPEC, family = "binomial", na.action = na.fail) #global w/o fire
+    dredge.globnofi <- dredge(mod.globnofi, rank = AIC, subset = dc(Elevation.m, Elevation.m2))
+    avg.globnofi <- model.avg(dredge.globnofi, subset = delta <= 2)
+  }
 
