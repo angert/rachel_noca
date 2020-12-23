054a3eca79f37ca2e4eca04934499d1ddbcffe3c
rachelnorawilson <rachelnora.wilson@gmail.com>
2020-12-23 01:34:31 -0800

    Extracting model averaging output and spitting into table

--- scripts/2_presence_analyses.R
+++ scripts/2_presence_analyses.R
@@ -14,6 +14,7 @@ library(MuMIn)
 
 und.presence <- read.csv("data/1_presence_with_fires.csv", header = TRUE, na.strings = "")
 und.presence$Fires <- as.factor(und.presence$Fires)
+und.presence$Data.Type <- as.factor(und.presence$Data.Type)
 load("data/Species.List.Rda") #TODO this file was made in an undocumented step
 species.list <- shifts$Species.Code[!shifts$Species.Code=="MOSS"] #removing "MOSS"
 species.list <- factor(species.list)
@@ -33,6 +34,7 @@ for(S in 1:length(species.list)) {
   # Create subset for species of interest S
   und.presence.SPEC <- subset(und.presence, Species.Code == levels(species.list)[S])
   und.presence.SPEC$Fires <- relevel(und.presence.SPEC$Fires, ref="Unburned")
+  und.presence.SPEC$Data.Type <- relevel(und.presence.SPEC$Data.Type, ref="Legacy")
   und.presence.SPEC$Elevation.m2 <- und.presence.SPEC$Elevation.m^2 #TODO better than poly()?
   und.presence.SPEC <- und.presence.SPEC[complete.cases(und.presence.SPEC), ] #Just in case
   
@@ -42,12 +44,11 @@ for(S in 1:length(species.list)) {
   
   #Emptying out previous objects
   mod.globfi <- NULL
-  dredge.globfi <- NULL
-  avg.glbofi <- NULL
   mod.globnofi <- NULL
+  dredge.globfi <- NULL
   dredge.globnofi <- NULL
-  avg.globnofi <- NULL
-  top.mods <- NULL
+  avg.mods <- NULL
+  top.mods.coeff <- NULL
   
   # If yes, include fire as a predictor in global model:
   if(num.burns["1", "Burned", "Legacy"] >= 5 | num.burns["1", "Burned", "Resurvey"] >= 5) {
@@ -59,8 +60,8 @@ for(S in 1:length(species.list)) {
                               dc(Elevation.m:Fires, Elevation.m2:Fires) &&
                               dc(Data.Type:Elevation.m:Fires, Data.Type:Elevation.m2:Fires), 
                             trace = 1)
-    avg.globfi <- model.avg(dredge.globfi, subset = delta <= 2)
-    top.mods.coeff <- as.data.frame(coef(subset(dredge.globnofi, delta <= 2)))
+    avg.mods <- model.avg(dredge.globfi, subset = delta <= 2)
+    top.mods.coeff <- as.data.frame(coef(subset(dredge.globfi, delta <= 2)))
   }
   
   # If no, exclude fire from global model:
@@ -71,7 +72,7 @@ for(S in 1:length(species.list)) {
                                 dc(Elevation.m, Elevation.m2) &&
                                 dc(Data.Type:Elevation.m, Data.Type:Elevation.m2), 
                               trace = 1)
-    avg.globnofi <- model.avg(dredge.globnofi, subset = delta <= 2)
+    avg.mods <- model.avg(dredge.globnofi, subset = delta <= 2)
     top.mods.coeff <- as.data.frame(coef(subset(dredge.globnofi, delta <= 2)))
   }
   
@@ -80,24 +81,56 @@ for(S in 1:length(species.list)) {
   mod.NULL <- glm(Pres.Abs ~ 1, 
               data = und.presence.SPEC, family = "binomial", na.action = na.fail)
   
+  # Adding NAs to missing coefficients in top.mods.coeff. Intercept should always be present
+  
+  coeff.all <- c("Data.TypeResurvey", "Elevation.m", "FiresBurned", "Data.TypeResurvey:Elevation.m", "Elevation.m:FiresBurned", "Data.TypeResurvey:FiresBurned", "Elevation.m2", "Data.TypeResurvey:Elevation.m2", "Elevation.m2:FiresBurned", "Data.TypeResurvey:Elevation.m:FiresBurned", "Data.TypeResurvey:Elevation.m2:FiresBurned")
+  if(is.na(top.mods.coeff["Data.TypeResurvey"]))
+    mod.top[[i]] $coeff ["Data.TypeResurvey"]<-0
+  
+  
+  is.na(top.mods.coeff$Data.TypeResurvey)
+  Elevation.m = top.mods.coeff$Elevation.m[i],
+  Fires = top.mods.coeff$FiresBurned[i],
+  Data.Type.Elevation.m = top.mods.coeff$`Data.TypeResurvey:Elevation.m`[i],
+  Elevation.m.Fires = top.mods.coeff$`Elevation.m:FiresBurned`[i],
+  Data.Type.Fires = top.mods.coeff$`Data.TypeResurvey:FiresBurned`[i],
+  Elevation.m2 = top.mods.coeff$Elevation.m2[i],
+  Data.Type.Elevation.m2 = top.mods.coeff$`Data.TypeResurvey:Elevation.m2`[i],
+  Elevation.m2.Fires = top.mods.coeff$`Elevation.m2:FiresBurned`[i],
+  Data.Type.Elevation.m.Fires = 
+    top.mods.coeff$`Data.TypeResurvey:Elevation.m:FiresBurned`[i],
+  Data.Type.Elevation.m2.Fires = 
+    top.mods.coeff$`Data.TypeResurvey:Elevation.m2:FiresBurned`[i],
+  
   # Storing output
   
   Mods.list.nofi <- list()
   
-  for(i in 1:length(top.mods)) {
+  for(i in 1:nrow(top.mods.coeff)) {
     Mods.list.nofi[[i]] <- data.frame(
                       Species = levels(species.list)[S],  
                       L.Occ = sum(num.burns["1", , "Legacy"]), 
                       R.Occ = sum(num.burns["1", , "Resurvey"]), 
                       Type = "Unavg", 
-                      deltaAIC = avg.globnofi$msTable$delta[i], 
-                      Weight = avg.globnofi$msTable$weight[i],
-                      Rsquared = 1 - avg.globnofi$msTable$logLik[i]/logLik(mod.NULL)[1],
+                      deltaAIC = avg.globfi$msTable$delta[i], 
+                      Weight = avg.globfi$msTable$weight[i],
+                      Rsquared = 1 - avg.globfi$msTable$logLik[i]/logLik(mod.NULL)[1],
                       Intercept = top.mods.coeff$`(Intercept)`[i],
-                      Data.Type = top.mods.coeff$Data.TypeResurvey[i], #TODO change to Data.Type
-                      Elevation.m = top.mods.coeff$Data.TypeResurvey[i],
+                      Data.Type = top.mods.coeff$Data.TypeResurvey[i],
+                      Elevation.m = top.mods.coeff$Elevation.m[i],
+                      Fires = top.mods.coeff$FiresBurned[i],
+                      Data.Type.Elevation.m = top.mods.coeff$`Data.TypeResurvey:Elevation.m`[i],
+                      Elevation.m.Fires = top.mods.coeff$`Elevation.m:FiresBurned`[i],
+                      Data.Type.Fires = top.mods.coeff$`Data.TypeResurvey:FiresBurned`[i],
                       Elevation.m2 = top.mods.coeff$Elevation.m2[i],
-                      DataType.Elevation.m = top.mods.coeff$Data.TypeResurvey:Elevation.m[i],
+                      Data.Type.Elevation.m2 = top.mods.coeff$`Data.TypeResurvey:Elevation.m2`[i],
+                      Elevation.m2.Fires = top.mods.coeff$`Elevation.m2:FiresBurned`[i],
+                      Data.Type.Elevation.m.Fires = 
+                        top.mods.coeff$`Data.TypeResurvey:Elevation.m:FiresBurned`[i],
+                      Data.Type.Elevation.m2.Fires = 
+                        top.mods.coeff$`Data.TypeResurvey:Elevation.m2:FiresBurned`[i],
+                      row.names = NULL)
+
   }
 }  
 
