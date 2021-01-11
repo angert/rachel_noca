b7d496292f09a2c6b8e8988bd9523566894161bf
rachelnorawilson <rachelnora.wilson@gmail.com>
2021-01-11 13:26:52 -0800

    Removing invasives and family-level IDs

--- scripts/2_rarefaction.R
+++ scripts/2_rarefaction.R
@@ -1,7 +1,6 @@
 # Created: Jan. 11, 2020
 # Updated: Jan. 11, 2020
 
-# This script will be used to created a RAREFIED dataset
 # This script will be used to created a RAREFIED dataset based on a dataset whittled to common species (those appearing in both surveys).
 
 #### STEP 1: Import data ####
@@ -13,3 +12,28 @@ load("data/Species.List.Rda") #TODO this file was made in an undocumented step
 species.list <- shifts$Species.Code[!shifts$Species.Code=="MOSS"] #removing "MOSS"
 species.list <- factor(species.list)
 
+#### STEP 2: Additional data tidying ####
+
+# How many species are in each dataset, pre-tidying?
+
+length(table(und.cover$Species.Code[und.cover$Data.Type == "Legacy"])) #219 species
+length(table(und.cover$Species.Code[und.cover$Data.Type == "Resurvey"])) #536 species
+
+## 2(a): Remove invasives and family-level IDs
+
+removal2A.cover <- und.cover[!und.cover$Species.Code == "XBOR" &    # X___ = family-level ID
+                               !und.cover$Species.Code == "XBRA" &
+                               !und.cover$Species.Code == "XLAM" &
+                               !und.cover$Species.Code == "XLIC" &  # Lichen
+                               !und.cover$Species.Code == "XORC" &
+                               !und.cover$Species.Code == "XPOA" &
+                               !und.cover$Species.Code == "XRAN" &
+                               !und.cover$Species.Code == "NA" &
+                               !und.cover$Species.Code == "BRTE" &  # Cheatgrass (invasive)
+                               !und.cover$Species.Code == "ABGR" &  #Abies grandis (tree)
+                               !und.cover$Species.Code == "LAMU" &
+                               !und.cover$Species.Code == "HYPE" &
+                               !und.cover$Species.Code == "POBU" &
+                               !und.cover$Species.Code == "TRDU" &
+                               !und.cover$Species.Code == "ACGR" &
+                               !und.cover$Species.Code == "ACMA", ]
