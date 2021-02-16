fefd21f401c81974cfad3da1f6dabfa4dafd648d
rachelnorawilson <rachelnora.wilson@gmail.com>
2021-02-15 18:54:12 -0800

    WIP - exploratory analyses.

--- scripts/4_presence_analysis.R
+++ scripts/4_presence_analysis.R
@@ -16,22 +16,30 @@ load("data/Species.List.Rda") #TODO this file was made in an undocumented step
 species.list <- shifts$Species.Code[!shifts$Species.Code=="MOSS"] #removing "MOSS"
 species.list <- factor(species.list)
 
-head(warn.ALLDAT)
-table(warn.ALLDAT$Has_warning, warn.ALLDAT$Dataset, warn.ALLDAT$Species)
-
+just.avg <- coeff.ALLDAT[coeff.ALLDAT$Type == "Avg", ]
+just.avg.fire <- just.avg[just.avg$Fire.Included == "Yes", ]
+table(just.avg.fire$Species, just.avg.fire$)
 
 #### STEP 2: Exploratory visualizations of warnings ####
 
 (numbered.species <- data.frame(Species=species.list, No.=rep(1:42)))
 
-warn.SPEC <- warn.ALLDAT[warn.ALLDAT$Species == "AMAL", ]
+warn.SPEC <- warn.ALLDAT[warn.ALLDAT$Species == "VASI", ]
 
 table(warn.SPEC$Has_warning, warn.SPEC$Dataset)
+table(table(warn.SPEC$Has_warning, warn.SPEC$Dataset))
+ nhead(warn.SPEC[warn.SPEC$Has_warning == TRUE, ])
 
 
 
+coeff.SPEC <- coeff.ALLDAT[coeff.ALLDAT$Species == "RHAL", ]
 
+# Exploring coefficients
+mean(coeff.SPEC$Data.Type.Elevation.m.Fires[coeff.SPEC$Type == "Avg"], na.rm = TRUE)
 
+#### STEP 3: How did rarefaction vary between species? ####
+par(mfrow=c(1,3), mar=c(5, 1.5, 1, 0), oma=c(0,4,0,0)) # 3-paneled graph
+hist(coeff.SPEC$R.Occ, main = "RHAL", xlab = "No. resurvey occ", ylab = "Frequency")
 
 
 
