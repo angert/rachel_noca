c5df1aacb205f638bc4ccf3cc8bde2d2d1587427
rachelnorawilson <rachelnora.wilson@gmail.com>
2021-02-16 17:10:17 -0800

    Corrected modeling framework to specify legacy, resurvey-burn, and resurvey-unburn

--- scripts/3_presence_modeling.R
+++ scripts/3_presence_modeling.R
@@ -20,18 +20,19 @@ df.fun <- function(ModID) {
   return(df)
 }
 
-# List of coefficients
-coeff.all <-c("Data.TypeResurvey", 
-              "Elevation.m", 
-              "FiresBurned", 
-              "Data.TypeResurvey:Elevation.m", 
-              "Elevation.m:FiresBurned", 
-              "Data.TypeResurvey:FiresBurned", 
+# List of coefficients between fire and non-fire model framework
+#TODO: confusing
+coeff.all <-c( "Elevation.m", 
               "Elevation.m2", 
-              "Data.TypeResurvey:Elevation.m2", 
-              "Elevation.m2:FiresBurned", 
-              "Data.TypeResurvey:Elevation.m:FiresBurned", 
-              "Data.TypeResurvey:Elevation.m2:FiresBurned")
+              "New.Data.TypeResurvey.Burned", 
+              "New.Data.TypeResurvey.Unburned", 
+              "Elevation.m:New.Data.TypeResurvey.Burned", 
+              "Elevation.m:New.Data.TypeResurvey.Unburned", 
+              "Elevation.m2:New.Data.TypeResurvey.Burned", 
+              "Elevation.m2:New.Data.TypeResurvey.Unburned",
+              "Data.TypeResurvey",
+              "Data.TypeResurvey:Elevation.m",
+              "Data.TypeResurvey:Elevation.m2")
 
 #### STEP 1: Import data #### Takes ~ 5 sec to run
 
@@ -39,14 +40,18 @@ coeff.all <-c("Data.TypeResurvey",
 # und.presence <- read.csv("data/1_presence_fires_unrarefied", header = TRUE, na.strings = "")
 
 # To analyze rarefied data (as list of dataframes named rare.ALL):
-load("data/rare.ALL.Rda")
+load("data/rare.ALL.Rda") # Run time 5 sec
 
 # Import for both data types:
 load("data/Species.List.Rda") #TODO this file was made in an undocumented step
 species.list <- shifts$Species.Code[!shifts$Species.Code=="MOSS"] #removing "MOSS"
 species.list <- factor(species.list)
 
-
+#TODO Formula to correct erroneous burn coding. Run time 3 sec
+for(D in 1:100) {
+  rare.ALL[[D]]$Fires[rare.ALL[[D]]$Data.Type == "Legacy"] <- rep("Unburned")
+  rare.ALL[[D]]$New.Data.Type <- paste(rare.ALL[[D]]$Data.Type, rare.ALL[[D]]$Fires, sep = ".")
+}
 
 
 #### STEP 2: Loop to analyze presence data ####
@@ -58,19 +63,22 @@ species.list <- factor(species.list)
 coeff.ALLDAT <- list()
 warn.ALLDAT <- list()
 
-#for(D in 1:100) { #RUN TIME: 32 minutes 51 sec
+for(D in 1:100) { #RUN TIME: 32 minutes 51 sec
   
   coeff.ALLSPEC <- list()
   warn.ALLSPEC <- list()
   und.presence <- rare.ALL[[D]]
+  und.presence$Elevation.m <- as.numeric(und.presence$Elevation.m)
+  und.presence$New.Data.Type <- factor(und.presence$New)
   und.presence$Fires <- as.factor(und.presence$Fires)
   und.presence$Data.Type <- as.factor(und.presence$Data.Type)
-  und.presence$Elevation.m <- as.numeric(und.presence$Elevation.m)
   
   for(S in 1:length(species.list)) { # RUN TIME: ~15-20 sec for 1 dataset
     
     # Create subset for species of interest S
     und.presence.SPEC <- subset(und.presence, Species.Code == levels(species.list)[S])
+    und.presence.SPEC$New.Data.Type <- relevel(und.presence.SPEC$New.Data.Type, 
+                                               ref="Legacy.Unburned")
     und.presence.SPEC$Fires <- relevel(und.presence.SPEC$Fires, ref="Unburned")
     und.presence.SPEC$Data.Type <- relevel(und.presence.SPEC$Data.Type, ref="Legacy")
     und.presence.SPEC$Elevation.m2 <- und.presence.SPEC$Elevation.m^2 #TODO better than poly()?
@@ -94,9 +102,9 @@ warn.ALLDAT <- list()
     sink(log.file, append = TRUE, type = "message")
     
     
-    # If burn > 5, include fire as a predictor in global model:
-    if(num.burns["1", "Burned", "Legacy"] >= 5 | num.burns["1", "Burned", "Resurvey"] >= 5) {
-      mod.globfi <- glm(Pres.Abs ~ Data.Type * (Elevation.m + Elevation.m2) * Fires, 
+    # If burn > 5, include fire as a predictor in global model (New.Data.Type):
+    if(num.burns["1", "Burned", "Resurvey"] >= 5) {
+      mod.globfi <- glm(Pres.Abs ~ (Elevation.m + Elevation.m2) * New.Data.Type, 
                         data = und.presence.SPEC, family = "binomial", na.action = na.fail)
       options(warn = -1) # Ignore warnings - not for logging.
       dredge.list <- lapply(dredge(mod.globfi, rank = AIC, subset = 
@@ -119,7 +127,7 @@ warn.ALLDAT <- list()
     
     
     #If burn < 5 in both, exclude fire from global model:
-    if(num.burns["1", "Burned", "Legacy"] < 5 & num.burns["1", "Burned", "Resurvey"] < 5) {
+    if(num.burns["1", "Burned", "Resurvey"] < 5) {
       mod.globnofi <- glm(Pres.Abs ~ Data.Type * (Elevation.m + Elevation.m2), 
                           data = und.presence.SPEC, family = "binomial", na.action = na.fail) 
       options(warn = -1) # Ignore warnings - not for logging.
@@ -185,19 +193,23 @@ warn.ALLDAT <- list()
         Weight = top.mods.coeff$weight[i], 
         Rsquared = 1 - top.mods.coeff$logLik[i] / as.numeric(logLik(mod.NULL)),
         Intercept = top.mods.coeff$`(Intercept)`[i],
-        Data.Type = top.mods.coeff$Data.TypeResurvey[i],
         Elevation.m = top.mods.coeff$Elevation.m[i],
-        Fires = top.mods.coeff$FiresBurned[i],
-        Data.Type.Elevation.m = top.mods.coeff$`Data.TypeResurvey:Elevation.m`[i],
-        Elevation.m.Fires = top.mods.coeff$`Elevation.m:FiresBurned`[i],
-        Data.Type.Fires = top.mods.coeff$`Data.TypeResurvey:FiresBurned`[i],
         Elevation.m2 = top.mods.coeff$Elevation.m2[i],
-        Data.Type.Elevation.m2 = top.mods.coeff$`Data.TypeResurvey:Elevation.m2`[i],
-        Elevation.m2.Fires = top.mods.coeff$`Elevation.m2:FiresBurned`[i],
-        Data.Type.Elevation.m.Fires = 
-          top.mods.coeff$`Data.TypeResurvey:Elevation.m:FiresBurned`[i],
-        Data.Type.Elevation.m2.Fires = 
-          top.mods.coeff$`Data.TypeResurvey:Elevation.m2:FiresBurned`[i],
+        Data.Type.nofi = top.mods.coeff$Data.TypeResurvey[i],
+        Data.Type.Elevation.m.nofi = top.mods.coeff$`Data.TypeResurvey:Elevation.m`[i],
+        Data.Type.Elevation.m2.nofi = top.mods.coeff$`Data.TypeResurvey:Elevation.m2`[i],
+        Resurvey.Burned.fi = 
+          top.mods.coeff$New.Data.TypeResurvey.Burned[i],
+        Resurvey.Unburned.fi = 
+          top.mods.coeff$New.Data.TypeResurvey.Unburned[i],
+        Elevation.m.Res.Burn.fi = 
+          top.mods.coeff$`Elevation.m:New.Data.TypeResurvey.Burned`[i],
+        Elevation.m.Res.Unburn.fi = 
+          top.mods.coeff$`Elevation.m:New.Data.TypeResurvey.Unburned`[i],
+        Elevation.m2.Res.Burn.fi = 
+          top.mods.coeff$`Elevation.m2:New.Data.TypeResurvey.Burned`[i],
+        Elevation.m2.Res.Unburn.fi = 
+          top.mods.coeff$`Elevation.m2:New.Data.TypeResurvey.Unburned`[i],
         row.names = NULL)
     }
     
@@ -221,27 +233,28 @@ warn.ALLDAT <- list()
       Weight = NA,
       Rsquared = NA,
       Intercept = sum(top.mods.zeroes$`(Intercept)` * top.mods.zeroes$weight),
-      Data.Type = sum(top.mods.zeroes$`Data.TypeResurvey` * top.mods.zeroes$weight),
       Elevation.m = sum(top.mods.zeroes$`Elevation.m` * top.mods.zeroes$weight),
-      Fires = sum(top.mods.zeroes$`FiresBurned` * top.mods.zeroes$weight),
-      Data.Type.Elevation.m =  sum(top.mods.zeroes$`Data.TypeResurvey:Elevation.m` * 
-                                     top.mods.zeroes$weight),
-      Elevation.m.Fires = sum(top.mods.zeroes$`Elevation.m:FiresBurned` * 
-                                      top.mods.zeroes$weight),
-      Data.Type.Fires = sum(top.mods.zeroes$`Data.TypeResurvey:FiresBurned` * 
-                              top.mods.zeroes$weight),
       Elevation.m2 = sum(top.mods.zeroes$`Elevation.m2` * top.mods.zeroes$weight),
-      Data.Type.Elevation.m2 = sum(top.mods.zeroes$`Data.TypeResurvey:Elevation.m2` * 
-                                     top.mods.zeroes$weight),
-      Elevation.m2.Fires = sum(top.mods.zeroes$`Elevation.m2:FiresBurned` * 
-                                   top.mods.zeroes$weight),
-      Data.Type.Elevation.m.Fires = 
-        sum(top.mods.zeroes$`Data.TypeResurvey:Elevation.m:FiresBurned` * 
-              top.mods.zeroes$weight),
-      Data.Type.Elevation.m2.Fires = 
-        sum(top.mods.zeroes$`Data.TypeResurvey:Elevation.m2:FiresBurned` * 
+      Data.Type.nofi = sum(top.mods.zeroes$`Data.TypeResurvey` * top.mods.zeroes$weight),
+      Data.Type.Elevation.m.nofi = sum(top.mods.zeroes$`Data.TypeResurvey:Elevation.m` * 
+                                         top.mods.zeroes$weight),
+      Data.Type.Elevation.m2.nofi = sum(top.mods.zeroes$`Data.TypeResurvey:Elevation.m2` * 
+                                          top.mods.zeroes$weight),
+      Resurvey.Burned.fi = 
+        sum(top.mods.zeroes$`New.Data.TypeResurvey.Burned` * top.mods.zeroes$weight),
+      Resurvey.Unburned.fi = 
+        sum(top.mods.zeroes$`New.Data.TypeResurvey.Unburned` * top.mods.zeroes$weight),
+      Elevation.m.Res.Burn.fi = 
+        sum(top.mods.zeroes$`Elevation.m:New.Data.TypeResurvey.Burned` * top.mods.zeroes$weight),
+      Elevation.m.Res.Unburn.fi = 
+        sum(top.mods.zeroes$`Elevation.m:New.Data.TypeResurvey.Unburned` * top.mods.zeroes$weight),
+      Elevation.m2.Res.Burn.fi = 
+        sum(top.mods.zeroes$`Elevation.m2:New.Data.TypeResurvey.Burned` * top.mods.zeroes$weight),
+      Elevation.m2.Res.Unburn.fi = 
+        sum(top.mods.zeroes$`Elevation.m2:New.Data.TypeResurvey.Unburned` * 
               top.mods.zeroes$weight),
       row.names = NULL)
+    
     
     coeff.ALLSPEC[[S]] <- rbind(Mods, Avg)
     
