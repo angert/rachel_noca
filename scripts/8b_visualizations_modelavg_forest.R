34daed9e43e0eeafe800b2a2fbb4ab7876a39ec7
Amy Angert <amyangert@amys-mbp-3.lan>
2022-02-09 17:17:18 -0800

    first attempt at forest plot. not working yet but major progress.

--- scripts/8b_visualizations_modelavg_forest.R
+++ scripts/8b_visualizations_modelavg_forest.R
@@ -12,18 +12,56 @@
 
 library(ggplot2)
 library(tidyverse)
+library(forestplot)
 
-####### PART 1: AVERAGE AVERAGED COEFFICIENTS #########
+####### PART 1: LOAD AND SUMMARIZE AVERAGED COEFFICIENTS FROM EACH RAREFACTION #########
 
-## Step 1: Loading and tidying average coefficient data
+## Step 1: Load coefficient data
 
 coeff.ALLDAT <- read.csv("data/3b_new_coefficients.csv", header = TRUE)
 coeff.ALLDAT[coeff.ALLDAT$Species == "EPAN", 1] <- paste("CHAN") # Correcting taxonomy issue
-coeff.fire <- 
-  coeff.ALLDAT[coeff.ALLDAT$Fire.Included == "Yes" & coeff.ALLDAT$Type == "Avg",
-                               c(1:12, 16:21)]
-coeff.nofire <- coeff.ALLDAT[coeff.ALLDAT$Fire.Included == "No" & coeff.ALLDAT$Type == "Avg",
-                               c(1:12, 13:15)]
+coeff.ALLDAT[is.na(coeff.ALLDAT)] <- 0 # Hard code absent coeffs as 0 before averaging
 
+coeff.fire <- #split by fire vs no-fire
+  coeff.ALLDAT[coeff.ALLDAT$Fire.Included == "Yes" & coeff.ALLDAT$Type == "Avg", c(1:12, 16:21)]
+coeff.nofire <- coeff.ALLDAT[coeff.ALLDAT$Fire.Included == "No" & coeff.ALLDAT$Type == "Avg", c(1:12, 13:15)]
 
+## Step 2: Summarize mean, lower and upper CI of each coefficient across the rarefactions
+means.fire <- coeff.fire %>% 
+  group_by(Species) %>% 
+  summarise(across(.cols = Elevation.m:Elevation.m2.Res.Unburn.fi, ~ unname(quantile(.x, 0.5)), .names = "mean_{.col}"))
+lowers.fire <- coeff.fire %>% 
+  group_by(Species) %>% 
+  summarise(across(.cols = Elevation.m:Elevation.m2.Res.Unburn.fi, ~ unname(quantile(.x, 0.025)), .names="lower_{.col}"))
+uppers.fire <- coeff.fire %>% 
+  group_by(Species) %>% 
+  summarise(across(.cols = Elevation.m:Elevation.m2.Res.Unburn.fi, ~ unname(quantile(.x, 0.975)), .names="upper_{.col}"))
 
+all.fire <- left_join(left_join(means.fire, lowers.fire), uppers.fire)
+
+ACMI <- all.fire %>% filter(Species=="ACMI")
+
+ACMI.mean <- ACMI %>% select(starts_with("mean")) %>% as.data.frame()
+ACMI.lower <- ACMI %>% select(starts_with("lower")) 
+ACMI.upper <- ACMI %>% select(starts_with("upper")) 
+
+base.data <- tibble(mean=ACMI.mean,
+                    lower=ACMI.lower,
+                    upper=ACMI.upper,
+                    study=c("Elevation", 
+                            "Elevation2", 
+                            "Burned",
+                            "Unburned",
+                            "Elevation*Burned",
+                            "Elevation*Unburned",
+                            "Elevation2*Burned", 
+                            "Elevation2*Unburned"))
+
+header <- tibble(study=c("Predictor"), 
+                 summary=FALSE)
+
+output_df <- bind_rows(header, base.data)
+
+output_df %>% forestplot(labeltext=study,
+                         is.summary=FALSE,
+                         xlog=FALSE)
