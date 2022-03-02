c97ee557077aac90c4a6ec9598e1592ac5b4e81a
Amy Angert <amyangert@amys-mbp-3.lan>
2022-03-02 12:20:53 -0800

    trying to examine elevation range of burns by species

--- scripts/9_visualizations_Elevation_w_fire.R
+++ scripts/9_visualizations_Elevation_w_fire.R
@@ -6,24 +6,43 @@
 
 # Packages needed:
 
-library(ggplot2)
+library(tidyverse)
 
-####### PART 1: Load data #########
+####### PART 1: All plots regardless of species #########
 
 load("data/list.fires.Rda")
 burn.data <- list.fires[list.fires$Data.Type == "Resurvey",]
 burn.data$Elevation.m <- as.numeric(burn.data$Elevation.m)
 
-
-
 violin.plot.burns <- ggplot(burn.data, aes(x = Fires, y = Elevation.m)) +
   geom_violin() +
   theme_classic() +
   xlab("") +
-  # scale_x_discrete(labels=c("Lower\nedge", "Range\ncenter", "Upper\nedge")) +
   ylim(100, 2200) +
   ylab("Elevation (m)") +
   theme(legend.position="none", text = element_text(size = 16)) +
   stat_summary(fun=mean, geom="point", cex=2)
 
+
+####### PART 2: By species #########
+
+species.list <- read.csv("data/3b_new_coefficients.csv", header = TRUE) %>% 
+  filter(Fire.Included=="Yes") %>% 
+  select(Species) %>% 
+  distinct()
+
+dat <- read_csv("data/1_presence_fires_unrarefied.csv") %>% 
+  filter(Pres.Abs==1) %>% 
+  filter(Data.Type=="Resurvey") %>% 
+  filter(Species.Code %in% species.list$Species)
+
+violin.plot.burns.spp <- ggplot(dat, aes(x = Fires, y = Elevation.m)) +
+  facet_wrap(~Species.Code) +
+  geom_violin() +
+  theme_classic() +
+  xlab("") +
+  ylim(100, 2200) +
+  ylab("Elevation (m)") +
+  theme(legend.position="none", text = element_text(size = 16)) +
+  stat_summary(fun=mean, geom="point", cex=2)
 
